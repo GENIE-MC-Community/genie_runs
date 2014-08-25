3aa2b3d5714ee034a4e9facc22c6132d86e1286d
Gabriel Perdue <perdue@fnal.gov>
2014-08-25 15:03:32 -0500

    Add a script to make splines for the VLE generator list.

---
+++ do_make_vle_spline.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Use just carbon-12 1000060120 and muon neutrinos.
+# See config/EventGeneratorListAssembler.xml for other valid channels.
+# Optionally supply an extra tag for the file name.
+
+XMLOUT=vle_carbon_splines
+if [ $# -gt 0 ]; then
+  XMLOUT=${XMLOUT}_$1
+fi
+XMLOUT=${XMLOUT}.xml
+echo "Making xml file $XMLOUT"
+
+nice gmkspl -p 14,-14 -t 1000060120 -o ${XMLOUT} --event-generator-list VLE
+
+
