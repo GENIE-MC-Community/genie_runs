1098ac68adc10bf91164a0cd1826189954d90fdf
Gabriel Perdue <perdue@fnal.gov>
2014-10-22 12:42:41 -0500

    Add a bit of taget documentation

--- do_a_vle_run.sh
+++ do_a_vle_run.sh
@@ -7,6 +7,14 @@ if [ $# -gt 0 ]; then
   NUMEVT=$1
 fi
 
-gevgen -n $NUMEVT -p -12 -t 1000060120 -e 0.02 -r 101 \
+# http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
+# http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
+# http://genie.hepforge.org/doxygen/html/PDGCodes_8h_source.html
+CARBON="1000060120"
+OXYGEN="1000080160"
+
+TARGET=$OXYGEN
+
+gevgen -n $NUMEVT -p -12 -t $TARGET -e 0.02 -r 101 \
   --seed 2989819 --cross-sections $XSECSPLINEDIR/gxspl-vA-v2.8.0.xml >& run_log.txt
 
