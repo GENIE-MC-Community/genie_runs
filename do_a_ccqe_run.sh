80d1c6344b5ceaa5c16ce5c8c3928c0a63d29fd1
Gabriel Perdue <perdue@fnal.gov>
2014-10-22 18:42:24 -0500

    Script for ccqe

---
+++ do_a_ccqe_run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Generic SN run...
+
+NUMEVT=10
+if [ $# -gt 0 ]; then
+  NUMEVT=$1
+fi
+
+# http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
+# http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
+# http://genie.hepforge.org/doxygen/html/PDGCodes_8h_source.html
+CARBON="1000060120"
+OXYGEN="1000080160"
+
+TARGET=$OXYGEN
+TARGET=$CARBON
+
+gevgen -n $NUMEVT -p -12 -t $TARGET -e 2 -r 101 \
+  --seed 2989819 --cross-sections $XSECSPLINEDIR/gxspl-vA-v2.8.0.xml \
+  --event-generator-list CCQE >& run_log.txt
+
