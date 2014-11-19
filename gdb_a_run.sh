e06d751d83ca2253b53b38ab80d48cead07fa378
Gabriel Perdue <perdue@fnal.gov>
2014-11-19 13:44:59 -0600

    gdb a generic run

---
+++ gdb_a_run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+NUMEVT=10
+if [ $# -gt 0 ]; then
+  NUMEVT=$1
+fi
+
+# http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
+# http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
+# http://genie.hepforge.org/doxygen/html/PDGCodes_8h_source.html
+CARBON="1000060120"
+OXYGEN="1000080160"
+
+TARGET=$OXYGEN
+
+gdb -tui --args gevgen -n $NUMEVT -p -14,14 -t $TARGET -e 2,10 -r 101 \
+  --seed 2989819 --cross-sections $XSECSPLINEDIR/gxspl-vA-v2.8.0.xml
