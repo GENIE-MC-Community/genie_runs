8cb2995f68f4b39ec590873e29bbf0d3bdb4bcfd
Gabriel Perdue <perdue@fnal.gov>
2014-12-02 11:26:40 -0600

    Add a script for single kaon splines

---
+++ do_make_single_kaon_spline.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+NKNOTS=100
+MAX_ENERGY=5
+CHANNEL="single_kaon"
+
+# http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
+# http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
+# http://genie.hepforge.org/doxygen/html/PDGCodes_8h_source.html
+PROTON="1000010010"
+CARBON="1000060120"
+OXYGEN="1000080160"
+ARGON40="1000180400"
+
+TARGET=$OXYGEN
+TARGET=$CARBON
+TARGET=$PROTON
+
+make_spline() {
+  # Optionally supply an extra tag for the file name.
+  XMLOUT=${CHANNEL}_${TARGET}_splines
+  if [ $# -gt 0 ]; then
+    XMLOUT=${XMLOUT}_$1
+  fi
+  # Add the file extension
+  XMLOUT=${XMLOUT}.xml
+  echo "Making xml file $XMLOUT"
+
+  nice gmkspl -p -14,-12,12,14 -t $TARGET -o $XMLOUT \
+  --event-generator-list SingleKaon \
+  -n $NKNOTS -e $MAX_ENERGY
+}
+
+TARGET=$PROTON
+make_spline
+
