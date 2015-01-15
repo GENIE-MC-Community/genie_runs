40d0efed53b328dda1365d10a17ce41f5375566f
Gabriel Perdue <perdue@fnal.gov>
2015-01-15 16:11:15 -0600

    make the res-spline-making script a bit more flexible

--- do_make_res_spline.sh
+++ do_make_res_spline.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-NKNOTS=100
-MAX_ENERGY=120
+NKNOTS=50
+MAX_ENERGY=100
+CHANNEL="resonance"
+EVGENLIST="RES"
 
 # http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
 # http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
@@ -12,16 +14,30 @@ OXYGEN="1000080160"
 ARGON40="1000180400"
 
 TARGET=$OXYGEN
-TARGET=$PROTON
 TARGET=$CARBON
+TARGET=$PROTON
 
-# Optionally supply an extra tag for the file name.
-XMLOUT=res_${TARGET}_splines
-if [ $# -gt 0 ]; then
-  XMLOUT=${XMLOUT}_$1
-fi
-XMLOUT=${XMLOUT}.xml
-echo "Making xml file $XMLOUT"
+NEUTRINOS="-14,-12,12,14"
+NEUTRINOS="12,14"
+NEUTRINOS="-14,14"
 
-nice gmkspl -p -14,-12,12,14 -t $TARGET -o ${XMLOUT} --event-generator-list RES \
+make_spline() {
+  # Optionally supply an extra tag for the file name.
+  XMLOUT=${CHANNEL}_${TARGET}_splines
+  if [ $# -gt 0 ]; then
+    XMLOUT=${XMLOUT}_$1
+  fi
+  # Add the file extension
+  XMLOUT=${XMLOUT}.xml
+  echo "Making xml file $XMLOUT"
+
+  nice gmkspl -p $NEUTRINOS -t $TARGET -o $XMLOUT \
+  --event-generator-list $EVGENLIST \
   -n $NKNOTS -e $MAX_ENERGY
+}
+
+# TARGET=$OXYGEN
+# make_spline
+
+TARGET=$CARBON
+make_spline
