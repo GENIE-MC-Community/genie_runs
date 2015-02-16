cd57113c036eb8922debadbe015c3c607b4fac0f
Gabriel Perdue <perdue@fnal.gov>
2015-02-16 09:52:58 -0600

    update the ccqe gdb script to point at eff spec func stuff for now

--- gdb_a_ccqe_run.sh
+++ gdb_a_ccqe_run.sh
@@ -7,6 +7,9 @@ if [ $# -gt 0 ]; then
   NUMEVT=$1
 fi
 
+SPLINEFILE=$XSECSPLINEDIR/gxspl-vA-v2.8.0.xml 
+SPLINEFILE=CCQE_TE_EFF_2_9_cand.xml
+
 # http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
 # http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
 # http://genie.hepforge.org/doxygen/html/PDGCodes_8h_source.html
@@ -15,8 +18,8 @@ OXYGEN="1000080160"
 
 TARGET=$OXYGEN
 
-gdb -tui --args gevgen -n $NUMEVT -p -12 -t $TARGET -e 2 -r 101 \
-  --seed 2989819 --cross-sections $XSECSPLINEDIR/gxspl-vA-v2.8.0.xml \
-  --message-thresholds Messenger_laconic.xml \
+gdb -tui --args gevgen -n $NUMEVT -p 14 -t $TARGET -e 1 -r 101 \
+  --seed 2989819 --cross-sections $SPLINEFILE \
+  --message-thresholds Messenger_whisper.xml \
   --event-generator-list CCQE
 
