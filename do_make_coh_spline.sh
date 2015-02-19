4cb137be2be37e4703156b33cc5b4e7741e70104
Gabriel Perdue <perdue@fnal.gov>
2015-02-19 15:04:09 -0600

    add some command line options to set the number of knots and max e

--- do_make_coh_spline.sh
+++ do_make_coh_spline.sh
@@ -4,7 +4,46 @@ NKNOTS=50
 MAX_ENERGY=100
 CHANNEL="coherent"
 EVGENLIST="COH"
+HELPFLAG=0
 
+help()
+{
+cat <<EOF
+Usage: ./do_make_coh_spline.sh -<f>|--<flag> arg
+                               -h / --help        : print the help menu
+                               -k / --knots #     : # of knots
+                               -e / --maxenergy # : max energy
+EOF
+}
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+    shift
+
+    case $key in
+        -h|--help)
+            HELPFLAG=1
+            ;;
+        -k|--knots)
+            NKNOTS="$1"
+            shift
+            ;;
+        -e|--maxenergy)
+            MAX_ENERGY="$1"
+            shift
+            ;;
+        *)     # Unknown option
+            ;;
+    esac
+done
+
+if [[ $HELPFLAG -eq 1 ]]; then
+  help
+  exit 0
+fi
+
+       
 # http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
 # http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
 # http://genie.hepforge.org/doxygen/html/PDGCodes_8h_source.html
