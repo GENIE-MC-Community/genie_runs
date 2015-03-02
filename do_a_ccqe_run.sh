1397e16766cbb460567c93f711117b5bff680850
Gabriel Perdue <perdue@fnal.gov>
2015-03-02 12:56:58 -0600

    Notes on doing EffSpecFun runs

--- do_a_ccqe_run.sh
+++ do_a_ccqe_run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Generic SN run...
-
 NUMEVT=10
 if [ $# -gt 0 ]; then
   NUMEVT=$1
@@ -17,7 +15,15 @@ OXYGEN="1000080160"
 TARGET=$OXYGEN
 TARGET=$CARBON
 
+SPLINEFILE=$XSECSPLINEDIR/gxspl-vA-v2.8.0.xml
+SPLINEFILE=CCQE_TE_EFF_2_9_cand.xml
+
+# To use Effective Spectral Functions, edit UserPhysicsOptions.xml so:
+#  <param type="alg" name="NuclearModel"> genie::EffectiveSF/Default </param>
+# is active. To active Transverse Enhancement, make sure:
+#  <param type="bool" name="UseElFFTransverseEnhancement"> true </param>
+
 gevgen -n $NUMEVT -p -12 -t $TARGET -e 2 -r 101 \
-  --seed 2989819 --cross-sections $XSECSPLINEDIR/gxspl-vA-v2.8.0.xml \
+  --seed 2989819 --cross-sections $SPLINEFILE \
   --event-generator-list CCQE >& run_log.txt
 
