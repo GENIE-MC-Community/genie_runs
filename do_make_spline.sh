964179525a2cce4615777d5ed0d6a9a8876aac9e
Gabriel Perdue <perdue@fnal.gov>
2015-03-06 14:18:10 -0600

    add single script to manage spline creation

---
+++ do_make_spline.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+NKNOTS=50
+MAX_ENERGY=100
+HELPFLAG=0
+
+# http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
+# http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
+# http://genie.hepforge.org/doxygen/html/PDGCodes_8h_source.html
+PROTON="1000010010"
+CARBON="1000060120"
+OXYGEN="1000080160"
+ARGON40="1000180400"
+
+TARGET=$PROTON
+NEUTRINOS="-14,14"
+LIST="Default"
+
+help()
+{
+cat <<EOF
+Usage: ./do_make_spline.sh -<f>|--<flag> arg
+                            -h / --help        : print the help menu
+                            -l / --list LIST   : interaction list (default all)
+                            -k / --knots #     : # of knots
+                            -e / --maxenergy # : max energy
+                            -t / --target #    : single target for now
+                            -n / --nus # or #1,#2, etc. (no spaces)
+
+* Possible interaction lists: (empty for all), CCQE, COH, RES, SingleKaon, VLE
+
+* Example targets: 1000060120 (Carbon) or 1000080160 (Oxygen) or etc.
+
+* Example neutrino lists: -14,14 or -14,-12,12,14 
+
+EOF
+}
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+    shift
+
+    case $key in
+        -h|--help)
+            HELPFLAG=1
+            ;;
+        -k|--knots)
+            NKNOTS="$1"
+            shift
+            ;;
+        -e|--maxenergy)
+            MAX_ENERGY="$1"
+            shift
+            ;;
+        -t|--target)
+            TARGET="$1"
+            shift
+            ;;
+        -n|--nus)
+            NEUTRINOS="$1"
+            shift
+            ;;
+        -l|--list)
+            LIST="$1"
+            shift
+            ;;
+        *)     # Unknown option
+            ;;
+    esac
+done
+
+if [[ $HELPFLAG -eq 1 ]]; then
+  help
+  exit 0
+fi
+
+       
+
+XMLOUT=${LIST}_${TARGET}_splines.xml
+echo "Making xml file $XMLOUT"
+
+echo "nice gmkspl -p $NEUTRINOS -t $TARGET -o $XMLOUT \\ "
+echo "  --event-generator-list $LIST \\ "
+echo "  -n $NKNOTS -e $MAX_ENERGY "
+
