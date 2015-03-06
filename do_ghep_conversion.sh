f136b749fea32e5d2ba157ef2c2294beaaff0f36
Gabriel Perdue <perdue@fnal.gov>
2015-03-06 16:00:37 -0600

    start and stop as arguments - needs work still

--- do_ghep_conversion.sh
+++ do_ghep_conversion.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-for i in 101 101
+for i in $1 $2
 do
   echo "$i"
-  gntpc -i ./gntp.$i.ghep.root -f rootracker
+  # gntpc -i ./gntp.$i.ghep.root -f rootracker
   gntpc -i ./gntp.$i.ghep.root -f gst
 done
 
