2d69b0c09cd3a55f28f7dbb4a4258e40c209bfa1
Gabriel Perdue <perdue@fnal.gov>
2015-03-06 13:49:55 -0600

    start to consolidate run and gdb scripts together

--- do_a_run.sh
+++ do_a_run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-NUMEVT=10
-if [ $# -gt 0 ]; then
-  NUMEVT=$1
-fi
+NUMEVT=100
+SPLINEFILE=$XSECSPLINEDIR/gxspl-NuMIsmall.xml 
+LIST="ALL"
+HELPFLAG=0
+SEED=2989819
 
 # http://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
 # http://pdg.lbl.gov/2011/mcdata/mc_particle_id_contents.html
@@ -11,12 +12,137 @@ fi
 PROTON="1000010010"
 CARBON="1000060120"
 OXYGEN="1000080160"
-
-TARGET=$OXYGEN
+ARGON40="1000180400"
 TARGET=$CARBON
+RUNNUM=101
+ENERGY=1
+NEUTRINOS="-14,14"
+GDB="NO"
 
-SPLINEFILE=gxspl-NuMIsmall.xml
 
-gevgen -n $NUMEVT -p -14,14 -t $TARGET -e 2,10 -f 'x*exp(-x)' -r 101 \
-  --seed 2989819 --cross-sections $XSECSPLINEDIR/$SPLINEFILE \
-  >& run_log.txt
+help()
+{
+cat <<EOF
+
+
+Usage: ./do_a_run.sh    -<f>|--<flag> arg
+                        -h / --help            : print the help menu
+                        -g / --gdb             : pipe the run through GDB
+                        -l / --list LIST       : interaction list (default all)
+                        -t / --target NUM      : default == 1000060120
+                        -n / --numevt NUM      : # of events
+                        -r / --run NUM         : run # (default 101)
+                        -e / --energy NUM(RNG) : e or emin,emax
+                        -n / --nus NU,NU,ETC   : neutrinos list (default -14,14)
+
+* Possible interaction lists: (empty for all), CCQE, COH, RES, SingleKaon, VLE
+
+* Possible targets: 1000060120 (Carbon), 1000080160 (Oxygen)
+
+* Example neutrino lists: -14,14 or -14,-12,12,14 
+
+* Example energies: 0.05 or 1,10
+
+* For splines, the script first searches: $XSECSPLINEDIR 
+
+* Channel specific splines are preferred to general splines if found.
+
+* Local (this directory) spline files are preferred to those located
+in $XSECSPLINEDIR.
+
+
+EOF
+}
+
+# Parse args
+while [[ $# > 0 ]]
+do
+    key="$1"
+    shift
+
+    case $key in
+        -h|--help)
+            HELPFLAG=1
+            ;;
+        -l|--list)
+            LIST="$1"
+            shift
+            ;;
+        -t|--target)
+            TARGET="$1"
+            shift
+            ;;
+        -n|--numevt)
+            NUMEVT="$1"
+            shift
+            ;;
+        -r|--run)
+            RUNNUM="$1"
+            shift
+            ;;
+        -e|--energy)
+            ENERGY="$1"
+            shift
+            ;;
+        -g|--gdb)
+            GDB="YES"
+            ;;
+        *)     # Unknown option
+            ;;
+    esac
+done
+
+if [[ $HELPFLAG -eq 1 ]]; then
+    help
+    exit 0
+fi
+
+
+if [[ $LIST == "ALL" ]]; then
+    if [[ -e gxspl-NuMIsmall.xml ]]; then
+        SPLINEFILE=gxspl-NuMIsmall.xml
+    fi
+elif [[ $LIST == "CCQE" || 
+              $LIST == "COH" ||
+              $LIST == "RES" ||
+              $LIST == "SingleKaon" ||
+              $LIST == "VLE" ]]; then    
+    FILENAM="$LIST_$TARGET_splines.xml"
+    if [[ -e $XSECSPLINEDIR/$FILENAM ]]; then
+        SPLINEFILE=$XSECSPLINEDIR/$FILENAM
+    fi
+    if [[ -e $FILENAM ]]; then
+        SPLINEFILE=$FILENAM
+    fi
+fi   
+         
+if [[ ! -e $SPLINEFILE ]]; then
+    echo "Spline file $SPLINEFILE does not exist!"
+    exit 1
+fi
+echo "Using spline file $SPLINEFILE"
+
+EVGENSTRING=""
+if [[ $LIST != "ALL" ]]; then
+    EVGENSTRING="--event-generator-list $LIST"
+fi
+
+if [[ "$GDB" == "YES" ]]; then
+
+    echo "gdb"
+    
+# gdb -tui --args gevgen -n $NUMEVT -p $NEUTRINOS -t $TARGET
+#     -e $ENERGY -r $RUNNUM \
+#     --seed 2989819 --cross-sections $SPLINEFILE \
+#     --message-thresholds Messenger_whisper.xml $EVGENSTRING
+
+else
+
+    echo "no gdb"
+    
+fi
+
+# gevgen -n $NUMEVT -p 12 -t $TARGET -e 0.01,0.03 -f 'x*exp(-x)' -r 101 \
+#   --seed 2989819 --cross-sections $SPLINEFILE \
+#   --event-generator-list VLE \
+#   >& run_log.txt
